65677f2882a1820dd30b7e8e61055740a41e5ba8
ViliM <ViliM@DESKTOP-M57IRI5>
2023-12-07 11:07:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GIK299_HT23_Vili_Mujkanovic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
